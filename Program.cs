15b20292c68a50279a47a9ee9632116e239b5cc7
ChouYS <ChouYS@DESKTOP-2D23C0S>
2025-10-15 08:50:10 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode_3516
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter x");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter y");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter z");
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The Answer:");
+            Console.WriteLine(FindClosest(a, b, c));
+
+
+            int FindClosest(int x, int y, int z)
+            {
+                int dX = x - z;
+                int dY = y - z;
+
+                if (dX < 0) dX = -dX;
+
+                if (dY < 0) dY = -dY;
+
+                if (dX < dY) return 1;
+                else if (dX > dY) return 2;
+                else return 0;
+
+            }
+        }
+    }
+}
